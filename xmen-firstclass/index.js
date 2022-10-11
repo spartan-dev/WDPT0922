40d35ee27e6ff5a0d4ac1a2e071a891261bb6697
kain <s.e.jugr@gmail.com>
2022-10-11 12:24:40 -0500

    feat: slack first flexbox class :skull:

---
+++ xmen-firstclass/index.js
@@ -0,0 +1,78 @@
+/**
+ * @Que es una variable
+ */
+let soyUnaVariable = "si lo soy";
+
+/**
+ * @Nombrando variables
+ */
+let miVariable = "camel case";
+let mi_segunda_variable = "soy la segunda variable";
+/**
+ * @Valores cambiantes
+ */
+let unXenLaVida = undefined; // puede cambiar de numero a letra si asi lo deseo
+/**
+ * @Variables con Const
+ */
+const noMeCambio = "este tipo no puede cmbiar es una constante";
+
+/**
+ * @Los tipos de data types
+ * primitivos
+ * objetos o no-primitivos
+ * 
+    number,
+    string,
+    boolean,
+    null,
+    undefined,
+    symbol (latest added in ECMAScript2015)
+    inmutables
+ */
+let soyUnNumero = 99;
+let soyUnString = "hola mundo";
+let isALive = false; //soy un booleano verdadero falso
+let soyNulo = null; // null no es vacio en si solo espero ser llenado con alguno de los otros valores
+let soyUndefined = undefined; // sin definir no significa vacio solo espera un valor mas adelante
+/**
+ * @expresiones de numeros
+ * son todas las operaciones basicas
+ *  + - / *
+ */
+const result = (7 + 5) / 3 - 8;
+//podemos agrupar como queramos y respeta la precedencia de operadores
+console.log(result);
+// => -4
+
+/**
+ * @Precendencia de operadores
+ */
+// como mencionamos toma la precedencia de operadores
+
+/**
+ * @Cadenas de texto Strings
+ */
+const teacherName = "Kain";
+const myAge = 39;
+const cadenaConstruida = `Esta es una cadena de texto con mi nombre ${teacherName} mi edad ${myAge}`;
+/**
+ * @Metodos de cadenas
+ */
+cadenaConstruida.length();
+
+/**
+ * @Data types boolean undefined null
+ */
+
+/**
+ * @operators
+ */
+
+/**
+ * @immutability
+ */
+
+/**
+ * @advanced numbers and methods
+ */
