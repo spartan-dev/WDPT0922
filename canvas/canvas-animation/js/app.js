034833753ac7c425b0a29ab58b6666bca4c0b17f
Salvador Emmanuel <s.e.jugr@gmail.com>
2022-10-26 17:48:20 -0500

    feat: canvas ready to set

---
+++ canvas/canvas-animation/js/app.js
@@ -0,0 +1,14 @@
+const game = {
+init(){
+    console.log("iniital")
+}
+};
+
+const loader = {
+
+};
+
+window.addEventListener("load", () => {
+    game.init();
+    console.log("iniciamos el pedo")
+});
